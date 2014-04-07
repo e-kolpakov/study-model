541c97bf4ea345a0e6f0472f4de07d68212f7cdb
John <eugeny.kolpakov@gmail.com>
2014-04-07 22:28:00 +0400

    minor renamings and typos

--- tests/simulation_engine/simulation_input_tests.py
+++ tests/simulation_engine/simulation_input_tests.py
@@ -7,17 +7,17 @@ from simulation_engine.simulation_input import SimulationInput
 
 
 class SimulationInputTests(unittest.TestCase):
-    def test_students_zero_knwoledge_for_each_missing_competency(self):
+    def test_students_zero_knowledge_for_each_missing_competency(self):
         sim_input = SimulationInput()
 
-        A = Competency('A')
-        B = Competency('B')
-        C = Competency('C')
-        sim_input.competencies.extend([A, B, C])
+        a = Competency('A')
+        b = Competency('B')
+        c = Competency('C')
+        sim_input.competencies.extend([a, b, c])
 
         student1 = Student("student1", {}, BehaviorGroup())
-        student2 = Student("student2", {A: 0.1}, BehaviorGroup())
-        student3 = Student("student3", {A: 0.1, B: 0.2, C: 0.3}, BehaviorGroup())
+        student2 = Student("student2", {a: 0.1}, BehaviorGroup())
+        student3 = Student("student3", {a: 0.1, b: 0.2, c: 0.3}, BehaviorGroup())
 
         sim_input.students.append(student1)
         sim_input.students.append(student2)
@@ -25,8 +25,8 @@ class SimulationInputTests(unittest.TestCase):
 
         sim_input.prepare()
 
-        self.assertSequenceEqual(student1.competencies, {A: 0.0, B: 0.0, C: 0.0})
-        self.assertSequenceEqual(student2.competencies, {A: 0.1, B: 0.0, C: 0.0})
-        self.assertSequenceEqual(student3.competencies, {A: 0.1, B: 0.2, C: 0.3})
+        self.assertSequenceEqual(student1.competencies, {a: 0.0, b: 0.0, c: 0.0})
+        self.assertSequenceEqual(student2.competencies, {a: 0.1, b: 0.0, c: 0.0})
+        self.assertSequenceEqual(student3.competencies, {a: 0.1, b: 0.2, c: 0.3})
 
 

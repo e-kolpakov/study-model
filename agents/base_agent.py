c88053c1281fac11cbac1f5348289b355ba94f0f
John <eugeny.kolpakov@gmail.com>
2014-04-21 21:56:12 +0400

    Default __str__, __unicode__ and __repr__ for agents

--- agents/base_agent.py
+++ agents/base_agent.py
@@ -14,3 +14,12 @@ class BaseAgent(object):
     @property
     def agent_id(self):
         return self._agent_id
+
+    def __str__(self):
+        return "Agent {0}.{1}".format(self.__class__, self.agent_id)
+
+    def __unicode__(self):
+        return u"Agent {0}.{1}".format(self.__class__, self.agent_id)
+
+    def __repr__(self):
+        return self.__str__()

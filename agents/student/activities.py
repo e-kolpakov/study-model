6086a3dcf7038f98fdc9146a153e28fd507f4c8c
Eugeny Kolpakov <eugeny.kolpakov@gmail.com>
2014-09-22 20:19:17 +0400

    Added check for available resources to study session activity

--- agents/student/activities.py
+++ agents/student/activities.py
@@ -22,13 +22,15 @@ class StudySessionStudentActivity(BaseStudentActivity):
         self._logger = logging.getLogger(self.__class__.__name__)
 
     def activate(self, student, length, get_resources_callback, choose_resource_callback):
-        resources = get_resources_callback()
         entered_session = student.env.now
         self._logger.debug("Student {name} study session of length {length} started at {time}".format(
             name=student.name, time=student.env.now, length=length
         ))
 
-        while student.env.now - entered_session < length and not student.check_stop_participation(resources):
+        resources = get_resources_callback()
+        while student.env.now - entered_session < length and \
+                not student.check_stop_participation(resources) \
+                and resources:
             self._logger.debug("Choosing a resource to study at {time}".format(time=student.env.now))
             resource_to_study = choose_resource_callback(resources)
             self._logger.info("Student {name}({id}): resource {resource_name}({resource_id}) chosen at {time}".format(
@@ -38,3 +40,4 @@ class StudySessionStudentActivity(BaseStudentActivity):
             ))
 
             yield student.env.process(student.study_resource(resource_to_study))
+            resources = get_resources_callback()

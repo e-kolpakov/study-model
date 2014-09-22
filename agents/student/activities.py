fbabed2c601db9cc765b9e2f04fe8190ebda1440
Eugeny Kolpakov <eugeny.kolpakov@gmail.com>
2014-09-22 20:14:20 +0400

    Moved study session and idle into activities. More activities to come.

--- agents/student/activities.py
+++ agents/student/activities.py
@@ -1,6 +1,40 @@
+import logging
+
 __author__ = 'e.kolpakov'
 
 
 class BaseStudentActivity:
     def __init__(self):
         pass
+
+
+class IdleStudentActivity(BaseStudentActivity):
+    def __init__(self):
+        super(IdleStudentActivity, self).__init__()
+
+    def activate(self, student, length):
+        yield student.env.timeout(length)
+
+
+class StudySessionStudentActivity(BaseStudentActivity):
+    def __init__(self):
+        super(StudySessionStudentActivity, self).__init__()
+        self._logger = logging.getLogger(self.__class__.__name__)
+
+    def activate(self, student, length, get_resources_callback, choose_resource_callback):
+        resources = get_resources_callback()
+        entered_session = student.env.now
+        self._logger.debug("Student {name} study session of length {length} started at {time}".format(
+            name=student.name, time=student.env.now, length=length
+        ))
+
+        while student.env.now - entered_session < length and not student.check_stop_participation(resources):
+            self._logger.debug("Choosing a resource to study at {time}".format(time=student.env.now))
+            resource_to_study = choose_resource_callback(resources)
+            self._logger.info("Student {name}({id}): resource {resource_name}({resource_id}) chosen at {time}".format(
+                name=student.name, id=student.agent_id,
+                resource_name=resource_to_study.name, resource_id=resource_to_study.agent_id,
+                time=student.env.now
+            ))
+
+            yield student.env.process(student.study_resource(resource_to_study))
--- agents/student/student.py
+++ agents/student/student.py
@@ -1,11 +1,13 @@
 import logging
 
 from agents.base_agents import IntelligentAgent
+from agents.student.activities import IdleStudentActivity, StudySessionStudentActivity
 from agents.student.behaviors.behavior_group import BehaviorGroup
 from agents.student.behaviors.knowledge_acquisition import AllDependenciesAcquisitionBehavior
 from agents.student.behaviors.resource_choice import RationalResourceChoiceBehavior, RandomResourceChoiceBehavior
 from agents.student.behaviors.stop_participation import CourseCompleteStopParticipationBehavior
 from agents.student.behaviors.study_period import QuarterHourRandomStudyPeriodBehavior
+from infrastructure.descriptors import TypedDescriptor, TypedDescriptorWithDefault
 from infrastructure.observers import Observer, DeltaObserver, observer_trigger, AgentCallObserver
 from simulation.result import ResultTopics
 
@@ -13,6 +15,9 @@ __author__ = 'e.kolpakov'
 
 
 class Student(IntelligentAgent):
+
+    idle_activity = TypedDescriptorWithDefault(IdleStudentActivity, 'idle_activity')
+    study_session_activity = TypedDescriptorWithDefault(StudySessionStudentActivity, 'study_session_activity')
 
     def __init__(self, name, knowledge, behavior, skill=None, **kwargs):
         """
@@ -40,6 +45,7 @@ class Student(IntelligentAgent):
 
     @property
     @Observer.observe(topic=ResultTopics.KNOWLEDGE_SNAPSHOT)
+    @Observer.observe(topic=ResultTopics.KNOWLEDGE_COUNT, converter=lambda x: len(x))
     @DeltaObserver.observe(topic=ResultTopics.KNOWLEDGE_DELTA, delta=lambda x, y: x - y)
     def knowledge(self):
         """
@@ -88,41 +94,15 @@ class Student(IntelligentAgent):
 
     def study(self):
         while not self.stop_participation_event.processed:
-            available_resources = self.resource_lookup_service.get_accessible_resources(self)
-            if not available_resources:
-                self._logger.warn("No resources available")
-                return
-
             study_session_length = self._behavior.study_period.get_study_period(self, self.env.now)
-            yield self.env.process(self._study_session(study_session_length, available_resources))
-
-            idle_session_length = self._behavior.study_period.get_idle_period(self, self.env.now)
-            yield self.env.process(self._idle_session(idle_session_length))
-
-    def _idle_session(self, idle_session_length):
-        self._logger.debug("Student {name} idle session of length {length} started at {time}".format(
-            name=self.name, time=self.env.now, length=idle_session_length
-        ))
-        yield self.env.timeout(idle_session_length)
-
-    def _study_session(self, study_session_length, resources):
-        entered_session = self.env.now
-        self._logger.debug("Student {name} study session of length {length} started at {time}".format(
-            name=self.name, time=self.env.now, length=study_session_length
-        ))
-        while self.env.now - entered_session < study_session_length:
-            self._logger.debug("Choosing a resource to study at {time}".format(time=self.env.now))
-            resource_to_study = self._choose_resource(resources)
-            self._logger.info("Student {name}({id}): resource {resource_name}({resource_id}) chosen ".format(
-                name=self.name, id=self.agent_id,
-                resource_name=resource_to_study.name, resource_id=resource_to_study.agent_id
+            yield self.env.process(self.study_session_activity.activate(
+                self, study_session_length,
+                get_resources_callback=lambda: self.resource_lookup_service.get_accessible_resources(self),
+                choose_resource_callback=lambda res: self._choose_resource(res)
             ))
 
-            yield self.env.process(self.study_resource(resource_to_study))
-
-            if self._stop_participation(resources):
-                self.stop_participation_event.succeed()
-                return
+            idle_session_length = self._behavior.study_period.get_idle_period(self, self.env.now)
+            yield self.env.process(self.idle_activity.activate(self, idle_session_length))
 
     @observer_trigger
     @AgentCallObserver.observe(topic=ResultTopics.RESOURCE_USAGE)
@@ -135,12 +115,16 @@ class Student(IntelligentAgent):
         knowledge_to_acquire = self._acquire_knowledge(resource)
         time_to_study = self._get_time_to_study(knowledge_to_acquire)
         yield self.env.timeout(time_to_study)
-        self._knowledge = self._knowledge | knowledge_to_acquire
+        self._add_knowledge(knowledge_to_acquire)
 
         self._logger.debug("Student {name}: Studying resource {resource_name} done at {time}".format(
             name=self.name, resource_name=resource.name, time=self.env.now
         ))
         return
+
+    @observer_trigger
+    def _add_knowledge(self, new_knowledge):
+        self._knowledge = self._knowledge | new_knowledge
 
     def _get_time_to_study(self, facts):
         """
@@ -164,12 +148,15 @@ class Student(IntelligentAgent):
         """
         return self._behavior.knowledge_acquisition.acquire_facts(self, resource)
 
-    def _stop_participation(self, available_resources):
+    def check_stop_participation(self, available_resources):
         """
         :type available_resources: tuple[Resource]
         :rtype: bool
         """
-        return self._behavior.stop_participation.stop_participation(self, self.curriculum, available_resources)
+        should_stop = self._behavior.stop_participation.stop_participation(self, self.curriculum, available_resources)
+        if should_stop:
+            self.stop_participation_event.succeed()
+        return should_stop
 
 
 class RationalStudent(Student):

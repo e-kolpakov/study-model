5ad33e7e16a5b1e1bd5520f7cf883517fdf09036
Eugeny Kolpakov <eugeny.kolpakov@gmail.com>
2014-09-23 18:19:31 +0400

    Fixed broken activity

--- agents/student/activities.py
+++ agents/student/activities.py
@@ -29,7 +29,7 @@ class StudySessionStudentActivity(BaseStudentActivity):
     def activate(self, length):
         entered = self.env.now
         self._logger.debug("Student {name} study session of length {length} started at {time}".format(
-            name=self._student.name, time=self.env.now, length=length
+            name=self._student, time=self.env.now, length=length
         ))
 
         choose_resource = self._student.behavior.resource_choice.choose_resource
@@ -44,14 +44,14 @@ class StudySessionStudentActivity(BaseStudentActivity):
             time = get_remaining_time(self.env.now)
             return time, res, stop
 
-        remaining_time, resources, stop_participation = get_loop_parameters()
-        while remaining_time > 0 and not stop_participation and resources:
+        remaining_time, resources, stop = get_loop_parameters()
+        while remaining_time > 0 and not stop and resources:
             resource_to_study = choose_resource(self._student, self._student.curriculum, resources, remaining_time)
             self._logger.info(
                 "Student {0}: resource {1} chosen at {2}".format(self._student, resource_to_study, self.env.now)
             )
             yield from study_resource(resource_to_study)
-            remaining_time, resources, stop_participation = get_loop_parameters()
+            remaining_time, resources, stop = get_loop_parameters()
 
-        if stop_participation:
+        if stop:
             self._student.stop_participation()

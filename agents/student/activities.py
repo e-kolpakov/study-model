936c8c4d2bf82149d3efa1eb6c5e0cf5d8afe4d7
Eugeny Kolpakov <eugeny.kolpakov@gmail.com>
2014-09-23 18:10:39 +0400

    Simplified study session activity - all external dependencies are now visible

--- agents/student/activities.py
+++ agents/student/activities.py
@@ -26,46 +26,31 @@ class StudySessionStudentActivity(BaseStudentActivity):
         super(StudySessionStudentActivity, self).__init__(student)
         self._logger = logging.getLogger(self.__class__.__name__)
 
-    @property
-    def student_behavior(self):
-        return self._student.behavior
-
-    def _choose_resource(self, available_resources, remaining_time):
-        return self.student_behavior.resource_choice.choose_resource(
-            self._student, self._student.curriculum, available_resources, remaining_time
-        )
-
-    def _get_remaining_time(self, entered, length):
-        return entered + length - self.env.now
-
-    def _check_stop_participation(self, available_resources):
-        return self.student_behavior.stop_participation.stop_participation(
-            self._student, self._student.curriculum, available_resources
-        )
-
     def activate(self, length):
         entered = self.env.now
         self._logger.debug("Student {name} study session of length {length} started at {time}".format(
             name=self._student.name, time=self.env.now, length=length
         ))
 
+        choose_resource = self._student.behavior.resource_choice.choose_resource
+        study_resource = self._student.study_resource
+        get_accessible_resources = self._student.resource_lookup_service.get_accessible_resources
+        stop_participation = self._student.behavior.stop_participation.stop_participation
+        get_remaining_time = lambda now: entered + length - now
+
         def get_loop_parameters():
-            res = self._student.resource_lookup_service.get_accessible_resources(self._student)
-            stop = self._check_stop_participation(res)
-            time = self._get_remaining_time(entered, length)
+            res = get_accessible_resources(self._student)
+            stop = stop_participation(self._student, self._student.curriculum, res)
+            time = get_remaining_time(self.env.now)
             return time, res, stop
 
         remaining_time, resources, stop_participation = get_loop_parameters()
         while remaining_time > 0 and not stop_participation and resources:
-            self._logger.debug("Choosing a resource to study at {time}".format(time=self.env.now))
-            resource_to_study = self._choose_resource(resources, remaining_time)
-            self._logger.info("Student {name}({id}): resource {resource_name}({resource_id}) chosen at {time}".format(
-                name=self._student.name, id=self._student.agent_id,
-                resource_name=resource_to_study.name, resource_id=resource_to_study.agent_id,
-                time=self.env.now
-            ))
-
-            yield self._student.env.process(self._student.study_resource(resource_to_study))
+            resource_to_study = choose_resource(self._student, self._student.curriculum, resources, remaining_time)
+            self._logger.info(
+                "Student {0}: resource {1} chosen at {2}".format(self._student, resource_to_study, self.env.now)
+            )
+            yield from study_resource(resource_to_study)
             remaining_time, resources, stop_participation = get_loop_parameters()
 
         if stop_participation:

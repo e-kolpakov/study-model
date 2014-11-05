920b1fd0a8d3732d7c978aa601ff5aa8c79d8a97
E. Kolpakov <eugeny.kolpakov@gmail.com>
2014-11-05 18:15:54 +0700

    Moved simulation output module to simulation package

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import logging
 import logging.config
 
 from log_config import log_config
-from simulation_output import output_results
+from simulation.simulation_output import output_results
 from simulation.simulation import Simulation
 from simulation.result import SimulationResult
 from simulation.simulation_input import get_simulation_input

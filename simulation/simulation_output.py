449641f66a1d3ee7e432946d0aec1e4711c985cf
Eugeny Kolpakov <eugeny.kolpakov@gmail.com>
2015-01-13 09:53:11 +0300

    Refactored simulation output for a dictionary-based configuration instead of constructor keyword arguments

--- simulation/simulation_output.py
+++ simulation/simulation_output.py
@@ -1,3 +1,4 @@
+from io import StringIO
 from collections import OrderedDict, defaultdict
 from itertools import groupby
 import json
@@ -10,80 +11,103 @@ def _sort_and_group(sequence, key):
 
 
 class OutputRenderer:
-    def __init__(self, output_stream, resourse_access=True, snapshots=True, deltas=True, counts=True):
+    def __init__(self, output_stream, conf=None):
         self._output_stream = output_stream
-        self.resourse_access = resourse_access
-        self.snapshots = snapshots
-        self.deltas = deltas
-        self.counts = counts
+        self.config = conf if conf else defaultdict(lambda: False)
 
-    def _print(self, value, newline=True):
-        self._output_stream.write(value)
+    def _print(self, value, output_stream=None, newline=True):
+        output_stream = output_stream if output_stream else self._output_stream
+        output_stream.write(value)
         if newline:
-            self._output_stream.write("\n")
+            output_stream.write("\n")
 
 
 class HumanReadableOutputRenderer(OutputRenderer):
+    def _handler_map(self):
+        return OrderedDict([
+            ('resourse_usage', self.print_resource_usages),
+            ('snapshots', self.print_snapshots),
+            ('deltas', self.print_deltas),
+            ('counts', self.print_counts),
+        ])
+
     def render(self, results):
-        if self.resourse_access:
-            self.print_resource_usages(results)
-        if self.snapshots:
-            self.print_snapshots(results)
-        if self.deltas:
-            self.print_deltas(results)
-        if self.counts:
-            self.print_counts(results)
+        for key, handler in self._handler_map().items():
+            if self.config.get(key, False):
+                self._print(handler(results))
 
     def print_resource_usages(self, results):
-        self._print("=== Resources START ===")
-        resource_usages = results.get_parameter(ResultTopics.RESOURCE_USAGE)
-        for agent_name, group in _sort_and_group(resource_usages, key=lambda record: record.agent.name):
-            for item in sorted(group, key=lambda record: record.time):
-                self._print("Student {student} used {resource} at {time}".format(
-                    resource=item.value.name, student=agent_name, time=item.time))
-        self._print("==== Resources END ====")
+        with StringIO() as result_stream:
+            self._print("=== Resources START ===", result_stream)
+            resource_usages = results.get_parameter(ResultTopics.RESOURCE_USAGE)
+            for agent_name, group in _sort_and_group(resource_usages, key=lambda record: record.agent.name):
+                for item in sorted(group, key=lambda record: record.time):
+                    self._print(
+                        "Student {student} used {resource} at {time}".format(
+                            resource=item.value.name, student=agent_name, time=item.time
+                        ),
+                        result_stream
+                    )
+            self._print("==== Resources END ====", result_stream)
+            return result_stream.getvalue()
 
     def print_snapshots(self, results):
-        self._print("=== Snapshots START ===")
-        snaps = results.get_parameter(ResultTopics.KNOWLEDGE_SNAPSHOT)
-        for item in snaps:
-            snapshot_str = ", ".join(list(map(str, sorted(item.value, key=lambda fact: fact.code))))
-            self._print(
-                "Student {name} at {time}: {snapshot}".format(name=item.agent.name, snapshot=snapshot_str, time=item.time))
-        self._print("==== Snapshots END ====")
+        with StringIO() as result_stream:
+            self._print("=== Snapshots START ===", result_stream)
+            snaps = results.get_parameter(ResultTopics.KNOWLEDGE_SNAPSHOT)
+            for item in snaps:
+                snapshot_str = ", ".join(list(map(str, sorted(item.value, key=lambda fact: fact.code))))
+                self._print(
+                    "Student {name} at {time}: {snapshot}".format(
+                        name=item.agent.name, snapshot=snapshot_str, time=item.time
+                    ),
+                    result_stream
+                )
+            self._print("==== Snapshots END ====", result_stream)
+            return result_stream.getvalue()
 
     def print_deltas(self, results):
-        self._print("===== Delta START =====")
-        delta = results.get_parameter(ResultTopics.KNOWLEDGE_DELTA)
-        for item in delta:
-            delta_str = ", ".join(list(map(str, item.value)))
-            self._print("Student {name} at {time}: {delta}".format(name=item.agent.name, delta=delta_str, time=item.time))
-        self._print("====== Delta END ======")
+        with StringIO() as result_stream:
+            self._print("===== Delta START =====", result_stream)
+            delta = results.get_parameter(ResultTopics.KNOWLEDGE_DELTA)
+            for item in delta:
+                delta_str = ", ".join(list(map(str, item.value)))
+                self._print(
+                    "Student {name} at {time}: {delta}".format(name=item.agent.name, delta=delta_str, time=item.time),
+                    result_stream
+                )
+            self._print("====== Delta END ======", result_stream)
+            return result_stream.getvalue()
 
     def print_counts(self, results):
-        self._print("===== Count START =====")
-        counts = results.get_parameter(ResultTopics.KNOWLEDGE_COUNT)
-        for item in counts:
-            self._print("Student {name} at {time}: {count}".format(name=item.agent.name, count=item.value, time=item.time))
-        self._print("====== Count END ======")
+        with StringIO() as result_stream:
+            self._print("===== Count START =====", result_stream)
+            counts = results.get_parameter(ResultTopics.KNOWLEDGE_COUNT)
+            for item in counts:
+                self._print(
+                    "Student {name} at {time}: {count}".format(name=item.agent.name, count=item.value, time=item.time),
+                    result_stream
+                )
+            self._print("====== Count END ======", result_stream)
+            return result_stream.getvalue()
 
 
 class JsonOutputRenderer(OutputRenderer):
-    def __init__(self, output_stream, resourse_access=True, snapshots=True, deltas=True, counts=True, pretty_print=True):
-        super().__init__(output_stream, resourse_access, snapshots, deltas, counts)
+    def __init__(self, output_stream, config=None, pretty_print=True):
+        super().__init__(output_stream, config)
         self._pretty_print = pretty_print
 
     def render(self, results):
         result = OrderedDict()
         handler_map = OrderedDict([
-            ('resourse_usage', (lambda: self.resourse_access, self.get_resourse_access)),
-            ('snapshots', (lambda: self.snapshots, self.get_snapshots)),
-            ('deltas', (lambda: self.deltas, self.get_deltas)),
-            ('counts', (lambda: self.counts, self.get_counts)),
+            ('resourse_usage', self.get_resourse_access),
+            ('snapshots', self.get_snapshots),
+            ('deltas', self.get_deltas),
+            ('counts', self.get_counts),
         ])
 
-        for key, (predicate, handler) in handler_map.items():
-            if predicate():
+        for key, handler in handler_map.items():
+            if self.config.get(key, False):
                 result[key] = handler(results)
         result = json.dumps(result, indent=2) if self._pretty_print else json.dumps(result)
         self._print(result)
@@ -102,7 +126,7 @@ class JsonOutputRenderer(OutputRenderer):
         resource_usages = results.get_parameter(ResultTopics.RESOURCE_USAGE)
         for time, group in _sort_and_group(resource_usages, key=lambda item: item.time):
             result[time] = {}
-            for resource, items in _sort_and_group(group, key=lambda item: item.value):
+            for resource, items in _sort_and_group(list(group), key=lambda item: item.value):
                 result[time][resource.name] = list(map(lambda item: item.agent.name, items))
         return result
 
@@ -115,5 +139,4 @@ class JsonOutputRenderer(OutputRenderer):
         return self.time_series(results, ResultTopics.KNOWLEDGE_DELTA, value_transform=value)
 
     def get_counts(self, results):
-        value = lambda facts: [fact.code for fact in facts]
         return self.time_series(results, ResultTopics.KNOWLEDGE_COUNT, value_transform=int)
